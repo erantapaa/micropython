d771a2246dfc47c900f1f22fcd8884ec2e9d1480
Rob Fowler <legal@mianos.com>
2015-11-22 21:20:26 +1100

    Added symbols for DS protocol

--- esp8266/esp_1wire.h
+++ esp8266/esp_1wire.h
@@ -26,6 +26,18 @@
 
 #ifndef _INCLUDED_ESP_1WIRE_H_
 #define _INCLUDED_ESP_1WIRE_H_
+
+#define CONVERT_T 0x44
+#define READ_SCRATCH_PAD 0xBE
+#define WRITE_SCRATCH_PAD 0x4E
+#define COPY_SCRATCH_PAD 0x48
+#define RECALL_E  0xB8
+#define READ_POWER_SUPPLY 0xB4
+#define READ_ROM 0x33
+#define MATCH_ROM 0x55
+#define SEARCH_ROM 0xF0
+#define SKIP_ROM 0xCC
+
 uint8_t ds_reset(pmap_t *pmp);
 pmap_t  *ds_init(int gpio);
 void ds_write(pmap_t *pmp, uint8_t v, int power);
