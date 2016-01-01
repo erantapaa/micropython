52b3ec14c8f2534e9eaf7ccc878f349fba44d751
Rob Fowler <rfo@mianos.com>
2016-01-01 11:23:33 +1100

    Added basic server

--- esp8266/scripts/srv.py
+++ esp8266/scripts/srv.py
@@ -1,10 +1,23 @@
 import esp
-import sys
+import ujson
 
-def recv(socket, data):
-    sys.stdout.write(data)
 
-socket = esp.socket()
-socket.onconnect(lambda s: s.onrecv(recv))
-socket.bind(('0.0.0.0', 2323))
-socket.listen(1)
+class ControlServer:
+    def recv(self, data):
+        print("date '%s'" % data)
+        try:
+            dd = ujson.loads(data)
+        except Exception:
+            print("exception on '%s'\n" % data)
+        else:
+            print("data '%s'\n" % str(dd))
+            if 'port' in dd and 'state' in dd:
+                print("port %s to state %s" % (str(dd['port']), dd['state']))
+
+    def __init__(self, port):
+        self.socket = esp.socket()
+        self.socket.onconnect(lambda sock: sock.onrecv(lambda sock, data: self.recv(data)))
+        self.socket.bind(('0.0.0.0', port))
+        self.socket.listen(1)
+
+cs = ControlServer(2323)
