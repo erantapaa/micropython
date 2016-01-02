d4dbddfb69036f544187a1d5f6165d95b79d858b
Rob Fowler <rfo@mianos.com>
2016-01-02 17:15:10 +1100

    interface to indicator changed

--- esp8266/scripts/indicator.py
+++ esp8266/scripts/indicator.py
@@ -1,40 +1,6 @@
 import esp
 
 
-class Indicator:
-    ON = 0
-    OFF = 1
-
-    def __init__(self, pin_setter):
-        self.pin_setter = pin_setter()
-        self.timer = None
-
-    def timer_target(self):
-        if self.state == self.ON:
-            self.pin_setter.off()
-            self.state = self.OFF
-        else:
-            self.pin_setter.on()
-            self.state = self.ON
-
-    def blink(self, period=100):
-        if self.timer:
-            self.timer.cancel()
-            self.timer = None
-        self.timer = esp.os_timer(lambda timer: self.timer_target(),
-                                  period=period)
-
-    def mode(self, state):
-        if self.timer:
-            self.timer.cancel()
-            self.timer = None
-        if state == self.ON:
-            self.pin_setter.on()
-        else:
-            self.pin_setter.off()
-        self.state = state
-
-
 class PinSet:
     def __init__(self):
         esp.gpio.p16_init()
@@ -45,3 +11,44 @@ class PinSet:
 
     def off(self):
         esp.gpio.p16_write(1)
+
+class Indicator:
+    ON = 0
+    OFF = 1
+
+    def __init__(self, pin_setter=PinSet):
+        self.pin_setter = pin_setter()
+        self.timer = None
+        self.state = self.OFF
+        self.ons = 0
+
+    def timer_target(self):
+        if self.state == self.ON:
+            self.pin_setter.off()
+            self.state = self.OFF
+        else:
+            self.pin_setter.on()
+            self.state = self.ON
+            self.ons += 1
+
+    def blink_cancel(self):
+        if self.timer:
+            self.timer.cancel()
+            self.timer = None
+
+    def blink(self, period=100):
+        self.blink_cancel()
+        self.timer = esp.os_timer(lambda timer: self.timer_target(),
+                                  period=period)
+
+    def on(self):
+        self.blink_cancel()
+        self.pin_setter.on()
+        self.state = self.ON
+        self.ons += 1
+
+    def off(self):
+        self.blink_cancel()
+        self.pin_setter.off()
+        self.state = self.OFF
+
