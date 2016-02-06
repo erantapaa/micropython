4cabf6a1df0e7925e9150e232cbf2f8e618e92fa
Rob Fowler <rfo@mianos.com>
2016-02-07 10:34:14 +1100

    Updated tm3 code for sonoff

--- esp8266/scripts/tm3.py
+++ esp8266/scripts/tm3.py
@@ -2,14 +2,86 @@ import esp
 import pyb
 import gc
 import network
-from indicator import Indicator
 from wifi_events import WifiEventManager
-from srv import ControlServer
+# from srv import ControlServer
 
 # button
 # smartconfig
 # server
 # gpio
+
+class Sonoff:
+    GREEN_LED = 13
+    RELAY = 12
+
+class PinSet:
+    def __init__(self, pin=16):
+        self.pin = pin
+        if pin == 16:
+            esp.gpio.p16_init()
+            esp.gpio.p16_write(1)
+        else:
+            self.pg = pyb.Pin(pin, pyb.Pin.OUT_PP)
+            self.pg.high()
+
+    def on(self):
+        if self.pin == 16:
+            esp.gpio.p16_write(0)
+        else:
+            self.pg.low()
+
+    def off(self):
+        if self.pin == 16:
+            esp.gpio.p16_write(1)
+        else:
+            self.pg.high()
+
+
+class Indicator:
+    ON = 0
+    OFF = 1
+
+    def __init__(self, pin_setter):
+        self.pin_setter = pin_setter
+        self.timer = None
+        self.state = self.OFF
+        self.ons = 0
+
+    def timer_target(self):
+        if self.state == self.ON:
+            self.pin_setter.off()
+            self.state = self.OFF
+        else:
+            self.pin_setter.on()
+            self.state = self.ON
+            self.ons += 1
+
+    def blink_cancel(self):
+        if self.timer:
+            self.timer.cancel()
+            self.timer = None
+
+    def blink(self, period=100):
+        self.blink_cancel()
+        self.timer = esp.os_timer(lambda timer: self.timer_target(),
+                                  period=period)
+
+    def on(self):
+        self.blink_cancel()
+        self.pin_setter.on()
+        self.state = self.ON
+        self.ons += 1
+
+    def off(self):
+        self.blink_cancel()
+        self.pin_setter.off()
+        self.state = self.OFF
+
+    def indicate(self, state):
+        if state:
+            self.on()
+        else:
+            self.off()
 # json
 #  (switch on 0, relay on 12 and green led on 13)
 
@@ -54,8 +126,6 @@ class SM:
         self.running = False
 
 
-
-
 class ButtonHandler:
     STATE_BUTTON_START = 0
     STATE_BUTTON_COLLECT = 1
@@ -64,19 +134,20 @@ class ButtonHandler:
     STATE_CLICK_CONFIRM_CONFIG = 1
     STATE_CLICK_IN_CONFIG = 2
 
-    def __init__(self, period=750):
+    def __init__(self, period=500):
         self.state = self.STATE_BUTTON_START
         self.start_time = 0
         self.period = period
         self.click_state = self.STATE_CLICK_START
-        self.indicator = Indicator()
+        self.indicator = Indicator(PinSet(Sonoff.GREEN_LED))
+        self.relay = PinSet(Sonoff.RELAY)
         self.smartconfig = SM(self.indicator)
         self.iostate = False
 
     def timer_finish(self):
-        #print("Events")
-        #for ii in self.events:
-        #    print(ii)
+#        print("Events", self.click_state)
+#        for ii in self.events:
+#            print(ii)
 
         zero_count = len([ii for ii in self.events if ii[0] == 0])
         if self.click_state == self.STATE_CLICK_START:
@@ -92,6 +163,7 @@ class ButtonHandler:
                 self.indicator.blink(period=500)
             else:
                 self.click_state = self.STATE_CLICK_START
+                self.indicator.blink_cancel()
         elif self.click_state == self.STATE_CLICK_IN_CONFIG:
             self.click_state = self.STATE_CLICK_START
 
@@ -99,8 +171,12 @@ class ButtonHandler:
 
     def handle(self, button_value):
         if button_value == 0:
-            self.iostate = not self.iostate
-            self.indicator.indicate(self.iostate)
+            if self.iostate is True:
+                self.iostate = False
+                self.relay.off()
+            else:
+                self.iostate = True
+                self.relay.on()
 
         if self.state == self.STATE_BUTTON_START:
             self.start_time = pyb.millis()
@@ -134,7 +210,7 @@ class TManager:
         self.dqueues = dict()
         self.bhandler = ButtonHandler()
         self.wem = WifiEventManager()
-        cs = ControlServer(data_cb=lambda json: self.web(json))
+        # cs = ControlServer(data_cb=lambda json: self.web(json))
 
     def add_q(self, name, qq):
         if name not in self.dqueues:
@@ -147,6 +223,7 @@ storage = [0 for kk in range(4)]
 bq = esp.queue(storage, os_task=tm.os_task)
 esp.gpio.attach(0, queue=bq, debounce=20)
 tm.add_q('button', bq)
+
 
 def jsh(data):
     print("got %s\n" % str(data))
